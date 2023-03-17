61e01954982d4c55b181e44ee3b4b005ce09f4bd
RaitisBertulis <125643206+RaitisBertulis@users.noreply.github.com>
2023-03-17 11:30:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -25,14 +25,11 @@ def main():
     n=0
     m=0
     n, m = map(int, input().split())
-
     # second line - data 
     # data - contains m integers t(i) - the times in seconds it takes any thread to process i-th job
     data = list(map(int, input().split()))
-
     # TODO: create the function
     result = parallel_processing(n,m,data)
-    
     # TODO: print out the results, each pair in it's own line
     for pair in result:
         print(pair[0], pair[1])
